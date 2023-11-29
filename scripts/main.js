90027972e996c0c2e80a084cc08465271faa924c
panda <p4nd4@posteo.de>
2023-11-29 17:51:24 +0100

    css: small improvements

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,12 @@ function createNavbar() {
   const homeLink = document.createElement('a');
   homeLink.href = '/index.html'
   homeLink.textContent = 'Home';
+  homeLink.style.marginRight = '10px'
 
   const postJokesLink = document.createElement('a');
   postJokesLink.href = '/pages/postJokes.html';
   postJokesLink.textContent = 'send jokes';
+  postJokesLink.style.marginRight = '10px'
 
   navbar.appendChild(homeLink);
   navbar.appendChild(postJokesLink);
